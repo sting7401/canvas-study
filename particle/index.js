a84fe4eaf805ed94e62aa4a39221d3f51323a867
KyeongHwanSong <sting7410@naver.com>
2023-02-15 00:20:40 +0900

    paticle, tail

--- particle/index.js
+++ particle/index.js
@@ -1,11 +1,13 @@
 import CanvasOption from "./js/canvasOption.js";
 import Particle from "./js/Particle.js";
-import { randomNumBetween } from "./js/utils.js";
+import Tail from "./js/Tail.js";
+import { randomNumBetween, hypotenuse } from "./js/utils.js";
 
 class Canvas extends CanvasOption {
   constructor() {
     super();
 
+    this.tails = [];
     this.particles = [];
   }
 
@@ -25,18 +27,29 @@ class Canvas extends CanvasOption {
     this.createParticle();
   }
 
-  createParticle() {
+  crateTail() {
+    const x = randomNumBetween(this.canvasWidth * 0.2, this.canvasWidth * 0.8);
+    const vy = this.canvasHeight * randomNumBetween(0.01, 0.015) * -1;
+    const color = '255,255,255';
+
+    this.tails.push(new Tail(x, vy, color));
+  }
+
+
+  createParticle(x, y, color) {
     const PARTICLE_NUM = 400;
-    const x = randomNumBetween(0, this.canvasWidth);
-    const y = randomNumBetween(0, this.canvasHeight);
+    // const x = randomNumBetween(0, this.canvasWidth);
+    // const y = randomNumBetween(0, this.canvasHeight);
 
     for (let i = 0; i < PARTICLE_NUM; i += 1) {
-      const r = randomNumBetween(0, 3);
+      const r = randomNumBetween(2, 100) * hypotenuse(innerWidth, innerHeight) * 0.0001;
       const angle = (Math.PI / 180) * randomNumBetween(0, 360);
 
       const vx = r * Math.cos(angle);
       const vy = r * Math.sin(angle);
-      this.particles.push(new Particle(x, y, vx, vy));
+
+      const opacity = randomNumBetween(0.6, 0.9);
+      this.particles.push(new Particle(x, y, vx, vy, opacity, color));
     }
   }
 
@@ -52,8 +65,21 @@ class Canvas extends CanvasOption {
       delta = now - then;
       if (delta < this.interval) return;
 
-      this.ctx.fillStyle = this.bgColor;
+      this.ctx.fillStyle = '#000';
       this.ctx.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
+
+
+      if(Math.random() < 0.03) this.crateTail();
+
+      this.tails.forEach((tail, index) => {
+        tail.update();
+        tail.draw();
+
+        if (tail.vy > -0.7) {
+          this.tails.splice(index, 1);
+          this.createParticle(tail.x, tail.y, tail.color);
+        } 
+      })
 
       this.particles.forEach((particle, index) => {
         particle.update();
